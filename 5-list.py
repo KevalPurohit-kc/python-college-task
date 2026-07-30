0bcbc083af6d7a760baeb7026565152778423fbf
Keval Purohit <kevalpurohit305@gmail.com>
2026-07-30 18:03:31 +0530

    Add files via upload

---
+++ 5-list.py
@@ -0,0 +1,14 @@
+li = ["BMW","Audi","Rolls-Royce","TATA",39,50]
+
+#only one item print
+print(li[2])
+
+#starting to ending istem print
+
+print(li[2:6])
+
+#Comprehension
+li2 = []
+for i in li:
+    li2.append(i)
+print(li2)
