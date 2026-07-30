e0f0307ff3aeef1b3cc1f57d4d44347b48b1e212
Keval Purohit <kevalpurohit305@gmail.com>
2026-07-30 17:13:58 +0530

    Add files via upload

---
+++ Typecasting.py
@@ -0,0 +1,20 @@
+var1 = 300
+var2 = "215"
+var3 = 50.96
+
+print("===Int to string===")
+val1 = str(var1)
+print(val1)
+print(type(val1))
+
+print("===String to Int===")
+val2 = int(var2)
+print(val2)
+print(type(val2))
+
+
+print("===float to int===")
+val3 = int(var3)
+print(val3)
+print(type(val3))
+
