c83d1f14b1365435c9bfdc90afdec400d64d9dce
Keval Purohit <kevalpurohit305@gmail.com>
2026-08-11 12:16:29 +0530

    Add files via upload

---
+++ 11-ifelse.py
@@ -0,0 +1,11 @@
+admin = input("Enter user name:")
+password = int(input("enter password:"))
+
+if (admin == 'adminlog' and password == 4542):
+    print("success login")
+
+else:
+    print("your user name and password are wrong")
+
+
+    
