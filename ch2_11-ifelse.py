1040d88d01bea55be8269e780db1a1f2246087fa
Keval Purohit <kevalpurohit305@gmail.com>
2026-08-11 13:11:12 +0530

    Update ch2_11-ifelse.py

--- ch2_11-ifelse.py
+++ ch2_11-ifelse.py
@@ -1,8 +1,16 @@
-admin = input("Enter user name:")
-password = int(input("enter password:"))
+info = int(input("select number 1 for login \n2 for register:"))
 
-if (admin == 'adminlog' and password == 4542):
-    print("success login")
+
+if (info == 1):
+    if (admin == 'adminlog' and password == 4542):
+        admin = input("Enter user name:")
+        password = int(input("enter password:"))
+        print("success login")
+elif(info == 2):
+    new = input("enter new user name :")
+    npass = int(input("enter new password:"))
+    print("success Register")
+
 
 else:
     print("your user name and password are wrong")
