2300900c169a610729864de8793cad6a7e03a784
Keval Purohit <kevalpurohit305@gmail.com>
2026-08-25 12:25:53 +0530

    Add files via upload

---
+++ ch3_3_math modul.py
@@ -0,0 +1,8 @@
+import math as ma
+
+print('number square root is',ma.sqrt(20))
+print('pi number is',ma.pi)
+print('power 20 and 2:',ma.pow(20,2))
+print('-3 Absolute value is',ma.fabs(-3))
+print('factorial of 2 :',ma.factorial(2))
+print('logarithm of a 20:',ma.log(20))
