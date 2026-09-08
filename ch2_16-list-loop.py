eb7d493942e5b17fbfbd0510307aab4c233e04ce
Keval Purohit <kevalpurohit305@gmail.com>
2026-09-08 12:58:53 +0530

    Add files via upload

---
+++ ch2_16-list-loop.py
@@ -0,0 +1,20 @@
+lis = [10, 41, 52, 41]
+
+print("Elements of List:")
+for i in lis:
+    print(i)
+
+text = "Keval"
+
+print("\nCharacters of String:")
+for ch in text:
+    print(ch)
+
+dic = {
+    "name": "Keval",
+    "age": 19
+}
+
+print("\nDictionary:")
+for key, value in dic.items():
+    print(key, ":", value)
