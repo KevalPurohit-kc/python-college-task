6a45f27c26628b981e0e3a36d79e174cb2f5a6e2
Keval Purohit <kevalpurohit305@gmail.com>
2026-09-21 21:09:49 +0530

    Update 3-operator.py

--- 3-operator.py
+++ 3-operator.py
@@ -1,24 +1,25 @@
-#Operators
-print('''Enter following any one number
-        1)Addition
-        2)Subtraction
-        3)Multiplication
-        4)Divition...''')
-value = int(input("enter a value:"))
-val1=int(input("enter digite 1:" ))
-val2=int(input("enter digite 2:" ))
-if( value == 1):
-    print("Sum is :" ,val1 + val2)
-elif (value == 2):
-    print("Subtraction is :", val1 - val2)
-elif(value == 3):
-    print("Multiplication is :" ,val1 * val2)
-elif(value == 4):
-    print("Divition is :", val1 / val2)
-else :
-    print("Your number is not valide")
+# Arithmetic Operators
 
-    
+a = 10
+b = 5
 
+print("=== Arithmetic Operators ===")
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
 
+# Relational Operators
 
+print("=== Relational Operators ===")
+print("a > b:", a > b)
+print("a < b:", a < b)
+print("a == b:", a == b)
+print("a != b:", a != b)
+
+# Logical Operators
+
+print("=== Logical Operators ===")
+print("a > 5 and b < 10:", a > 5 and b < 10)
+print("a < 5 or b < 10:", a < 5 or b < 10)
+print("not(a > b):", not(a > b))
